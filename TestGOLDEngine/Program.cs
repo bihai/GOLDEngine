60779825dc4e50927fe10eb0f4de9c8ebfd28d07
Christopher Wells <cwellsx@gmail.com>
2012-09-24 09:09:19 -0400

    Component tests for the GOLDEngine project
    This is an executable which uses (exercises and tests) GOLDEngine.dll
    
    Run this executable, to test GOLDEngine.dll
    
    Add further tests to this project, to improve GOLDEngine testing and
    regression testing.

---
+++ TestGOLDEngine/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+
+namespace TestGOLDEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            firstTest();
+        }
+
+        static void assert(bool b)
+        {
+            if (!b)
+                throw new ApplicationException();
+        }
+
+        static void firstTest()
+        {
+            // Use the definition of the GOLD meta-language, to test itself.
+            WrapGOLDEngine wrapper;
+            using (Stream grammar = new FileStream("Tests\\GOLD Meta-Language (2.6.0).egt", FileMode.Open))
+            {
+                wrapper = new WrapGOLDEngine(grammar);
+            }
+            using (TextReader content = new StreamReader("Tests\\GOLD Meta-Language (2.6.0).grm"))
+            {
+                GOLDEngine.Reduction reduction = wrapper.Parse(content);
+                assert(reduction != wrapper.Parse(content));
+                //reduction[0].
+            }
+        }
+    }
+}
